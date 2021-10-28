87dcd7ec1d1be68396e6023c1048581c79db3dd5
amit1992luthra <34670873+amit1992luthra@users.noreply.github.com>
2021-10-28 16:57:07 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello Shell";
+        return "Day 4 Shell Batch";
     }
 
 }
